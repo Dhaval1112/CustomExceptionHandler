4a93018fb60f15b252327e346dd39d1b6fd1f7ad
JDBrothers1016 <jp1016charusat@gmail.com>
2022-03-03 13:42:17 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CustomExceptionHandler.Filters;
+using CustomExceptionHandler.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomExceptionHandler.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [MyCustomExceptionFilterHandler]
+        public IActionResult Index()
+        {
+            int num1 = 22;
+            int num2 = 0;
+            int num3 = num1 / num2;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            var exceptionPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            return View(exceptionPathFeature.Error);
+        }
+    }
+}
---
+++ Filters/MyCustomExceptionFilterHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomExceptionHandler.Filters
+{
+    public class MyCustomExceptionFilterHandler : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            ViewResult result = new ViewResult();
+            result.ViewName = "/Views/Shared/ErrorMessage.cshtml";
+            result.ViewData = new Microsoft.AspNetCore.Mvc.ViewFeatures.ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary());
+            result.ViewData.Model = filterContext.Exception;
+            result.ViewData["Controller"] = (string) filterContext.RouteData.Values["controller"];
+            result.ViewData["Action"] = (string) filterContext.RouteData.Values["action"];
+
+            filterContext.Result = result;
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
